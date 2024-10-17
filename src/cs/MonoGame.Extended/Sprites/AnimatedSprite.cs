c233b5db34d962221fe63a5b4a5fe8a0e7f2710b
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2024-10-17 01:24:24 -0500

    Added some more optimizations to eliminate garbage collection deallocations.

--- src/cs/MonoGame.Extended/Sprites/AnimatedSprite.cs
+++ src/cs/MonoGame.Extended/Sprites/AnimatedSprite.cs
@@ -7,7 +7,17 @@ namespace MonoGame.Extended.Sprites
     public class AnimatedSprite : Sprite
     {
         private readonly SpriteSheet _spriteSheet;
-        private SpriteSheetAnimation _currentAnimation;
+        private SpriteSheetAnimation _currentAnimation;        
+        private class Functor0
+        {
+            public SpriteSheet SpriteSheet;
+            public readonly Func<SpriteSheetAnimationFrame, TextureAtlases.TextureRegion2D> Func;
+            public Functor0()
+            {
+                Func = f => SpriteSheet.TextureAtlas[f.Index];
+            }
+        }
+        private static readonly Functor0 _functor0 = new();
 
         public AnimatedSprite(SpriteSheet spriteSheet, string playAnimation = null)
             : base(spriteSheet.TextureAtlas[0])
@@ -23,7 +33,8 @@ namespace MonoGame.Extended.Sprites
             if (_currentAnimation == null || _currentAnimation.IsComplete || _currentAnimation.Name != name)
             {
                 var cycle = _spriteSheet.Cycles[name];
-                var keyFrames = cycle.Frames.Select(f => _spriteSheet.TextureAtlas[f.Index]).ToArray();
+                _functor0.SpriteSheet = _spriteSheet;
+                var keyFrames = cycle.Frames.Select(_functor0.Func).ToArray();
                 _currentAnimation = new SpriteSheetAnimation(name, keyFrames, cycle.FrameDuration, cycle.IsLooping, cycle.IsReversed, cycle.IsPingPong);
 
                 if(_currentAnimation != null)
